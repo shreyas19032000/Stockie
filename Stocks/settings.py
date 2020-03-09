38dc0c5a5fac5106791e2a183f7ad73fc307aa9c
atharva waze <you@example.com>
2020-03-10 01:53:00 +0530

    News App started

--- Stocks/settings.py
+++ Stocks/settings.py
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
+    'news',
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
